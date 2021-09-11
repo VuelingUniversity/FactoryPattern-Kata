91de61468d24d78fcfcecaabb1f7f1dbfa9e90dc
tdom91 <tomasdominik@outlook.com>
2021-09-11 10:29:30 +0200

    Integrar CheckHardware,checknohardware y checkfactory en LicenseManager

--- src/FactoryPattern-Kata/Core/LicenseManager.cs
+++ src/FactoryPattern-Kata/Core/LicenseManager.cs
@@ -1,46 +1,39 @@
-﻿namespace FactoryPattern_Kata
+﻿using System.Collections.Generic;
+
+namespace FactoryPattern_Kata
 {
+	public interface IChecker
+	{
+		bool Check(ActivationData activation_data, License license_data);
+	}
+
+	public class CheckerFactory
+	{
+
+
+		public static IChecker CreateChecker(LicenseType type)
+		{
+			
+		}
+	}
+
+	public class CheckerHardware : IChecker
+	{
+
+	}
+
+	public class CheckerNoHardware : IChecker
+	{
+
+	}
+
 	public class LicenseManager
 	{
-		public interface IChecker
-		{
-			bool Check(ActivationData activation_data, License license_data);
-		}
 		public static bool CheckActivation(ActivationData activationData, License license)
 		{
-			//change the solution so the following code will work instead of the uncommented one , lines 12-30, that should be gone )
-			/*
-				IChecker checker = CheckerFactory.CreateChecker(license.LicenseType);
-				checker.Check(activationData, license);
-			 */
-			if (license.LicenseType == LicenseType.NoCheck)
-			{
-				if (activationData.Date > license.MaxActivations)
-				{
-					return false;
-				}
-				else
-				{
-					return true;
-				}
-			}
-			else if (license.LicenseType == LicenseType.Check)
-			{
-				if (activationData.Date > license.MaxActivations)
-					return false;
-				else if (license.Activations.Count != license.LimitOfActivations)
-					return true;
-				else if (!license.Activations.Contains(activationData.HardwareId))
-					return false;
-				else
-				{
-					return true;
-				}
-			}
-			else
-			{
-				return false;
-			}
+			var checker = CheckerFactory.CreateChecker(license.LicenseType);
+
+			return checker.Check(activationData, license);
 		}
 	}
 }
