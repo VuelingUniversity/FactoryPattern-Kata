5ca0a77552c60d3e585cc3bf869aeeef6ce8effb
SantiComeras <scomeras@DESKTOP-DUR62V1>
2021-09-13 09:48:11 +0200

    Factory Pattern v1

---
+++ src/FactoryPattern-Kata/Models/LicenseManager.cs
@@ -0,0 +1,12 @@
+﻿namespace FactoryPattern_Kata
+{
+	public class LicenseManager
+	{
+		public static bool CheckActivation(ActivationData activationData, License license)
+		{
+			//change the solution so the following code will work instead of the uncommented one , lines 12-30, that should be gone )
+			IChecker checker = CheckerFactory.CreateChecker(license.LicenseType);
+			return checker.Check(activationData, license);
+		}
+	}
+}
--- src/FactoryPattern-Kata/Models/LicenseType.cs
+++ src/FactoryPattern-Kata/Models/LicenseType.cs
@@ -1,6 +1,5 @@
 ﻿namespace FactoryPattern_Kata
 {
-	//this is a useless line
 	public enum LicenseType
 	{
 
